ac4a3646f5121d6f65d11b61235b3134d1bf4139
Erika <erikaadisti1308@example.com>
2025-06-18 11:32:30 +0700

    Menambahkan modal keranjang

--- js/script.js
+++ js/script.js
@@ -32,10 +32,34 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
   if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
-    searchForm.classList;
+    searchForm.classList.remove("active");
   }
 
   if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
     shoppingCart.classList.remove("active");
   }
 });
+
+// modal box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// klik tombol close
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+// klik diluar modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
